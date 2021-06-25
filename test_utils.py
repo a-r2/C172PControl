bfc917fa94a02aaf6b961187a52f008fc1d25012
a-r2 <82704603+a-r2@users.noreply.github.com>
2021-06-25 22:47:32 +0200

    Delete test_utils.py

--- test_utils.py
+++
@@ -1,9 +0,0 @@
-import math
-from utils import *
-
-angle = math.pi/2
-vector1 = [1,0,0]
-vector2 = body_to_stability(angle).rotate(vector1)
-
-print('vector1: ' + str(vector1))
-print('vector2: ' + str(vector2))
